96c8646fb70916324d2cfc8682aaf5d6994eaa6f
luisfelipedrs <felipe.drs01@gmail.com>
2023-02-06 12:57:41 -0300

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trello-app1
+//
+//  Created by Luis Felipe on 06/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
